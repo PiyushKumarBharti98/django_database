c08b4030398f7e703337d77b1078252ed75cf02a
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-02-22 22:15:37 +0530

    Update settings.py

--- quizapp/quizapp/settings.py
+++ quizapp/quizapp/settings.py
@@ -35,7 +35,14 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 # Application definition
 
 INSTALLED_APPS = [
