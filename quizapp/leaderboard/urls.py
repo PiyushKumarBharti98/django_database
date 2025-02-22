3159f9a2c4235ff16918ef0f2a7212d066545db4
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-02-22 23:52:46 +0530

    Update urls.py

--- quizapp/leaderboard/urls.py
+++ quizapp/leaderboard/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('leaderboard/<int:pk>/', views.LeaderboardDetailView.as_view(), name='leaderboard-detail'),
     path('leaderboard/<str:genre>/', views.GenreLeaderboardView.as_view(), name='genre-leaderboard'),
     path('leaderboard/<str:genre>/delete/<str:wallet_address>/', views.DeleteUserFromGenreView.as_view(), name='delete-user-from-genre'),
-     path('add-attempt/', views.AddQuizAttemptView.as_view(), name='add-quiz-attempt'),
+    path('add-attempt/', views.AddQuizAttemptView.as_view(), name='add-quiz-attempt'),
     path('total-score/<str:wallet_address>/<str:genre>/', views.UserTotalScoreView.as_view(), name='user-total-score'),
 ]
