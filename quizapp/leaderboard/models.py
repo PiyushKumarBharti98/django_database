dddd3e6d307bbd024d38e444bb48f491c9a5ee04
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-02-22 23:50:19 +0530

    Update models.py

--- quizapp/leaderboard/models.py
+++ quizapp/leaderboard/models.py
@@ -2,13 +2,10 @@
 from django.db import models
 
 class Leaderboard(models.Model):
-    wallet_address = models.CharField(max_length=42, unique=False)  # Ethereum wallet address
+    wallet_address = models.CharField(max_length=42)  # Ethereum wallet address
     genre = models.CharField(max_length=100)  # Genre of the quiz (e.g., "Harry Potter")
-    score = models.IntegerField()  # User's score
+    score = models.IntegerField()  # Score for this specific quiz attempt
     timestamp = models.DateTimeField(auto_now_add=True)  # Timestamp of the quiz attempt
-
-    class Meta:
-        unique_together = ('wallet_address', 'genre')  # Ensure one score per wallet per genre
 
     def __str__(self):
         return f"{self.wallet_address} - {self.genre} - {self.score}"
