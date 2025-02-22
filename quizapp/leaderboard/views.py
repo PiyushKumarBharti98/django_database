57ffe9c3ced41245e8ce0a77fc465c18703a38e5
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-02-22 21:51:16 +0530

    Update views.py

--- quizapp/leaderboard/views.py
+++ quizapp/leaderboard/views.py
@@ -2,6 +2,7 @@
 from rest_framework import generics
 from .models import Leaderboard
 from .serializers import LeaderboardSerializer
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class LeaderboardListCreateView(generics.ListCreateAPIView):
@@ -18,3 +19,17 @@ class GenreLeaderboardView(generics.ListAPIView):
     def get_queryset(self):
         genre = self.kwargs['genre']  # Get the genre from the URL
         return Leaderboard.objects.filter(genre=genre).order_by('-score')
+
+class DeleteUserFromGenreView(APIView):
+    def delete(self, request, genre, wallet_address):
+        try:
+            # Find the entry for the specified genre and wallet address
+            entry = Leaderboard.objects.get(genre=genre, wallet_address=wallet_address)
+            entry.delete()  # Delete the entry
+            return Response(
+                {"message": "User deleted successfully from the genre leaderboard."},
+            )
+        except Leaderboard.DoesNotExist:
+            return Response(
+                {"error": "User not found in the specified genre leaderboard."},
+            )
