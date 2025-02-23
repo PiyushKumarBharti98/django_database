ed93ef231278eb6dd3e708149b06780016e1f3e6
Piyush Kumar Bharti <contact.piyushkumarbharti@gmail.com>
2025-02-23 07:35:56 +0530

    Update serializers.py

--- quizapp/leaderboard/serializers.py
+++ quizapp/leaderboard/serializers.py
@@ -1,8 +1,14 @@
 # leaderboard/serializers.py
 from rest_framework import serializers
 from .models import Leaderboard
+from .models import GeneratedImage
 
 class LeaderboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Leaderboard
         fields = ['wallet_address', 'genre', 'score', 'timestamp']
+
+class GeneratedImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GeneratedImage
+        fields = ['id', 'prompt', 'image', 'created_at']
